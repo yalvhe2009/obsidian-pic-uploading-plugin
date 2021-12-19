c77a9e61dd3cf58bf2b59d1d03f0b62035f0b61e
杨德明 <yalvhe2009@outlook.com>
2021-12-19 17:07:40 +0800

    fix: notification incorrect when runing delete command

--- src/impls/aliOssUploadImpl.ts
+++ src/impls/aliOssUploadImpl.ts
@@ -53,37 +53,39 @@ export class AliOssImpl implements PUOss {
             success: true
         };
         let delNum: number = 0;
+        let matchNum: number = 0;//正则表达式匹配到的个数
         //把格式：![](http://ali.com/a/b/c.jpg)里的：/a/b/c.jpg捕获出来
         const regex = /!\[.*\]\(http(?:s)?:\/\/(?:[A-z0-9][A-z0-9\-]{0,61}[A-z0-9].)+[A-z]{2,6}(\/.*)\)/gm;
         let m;
-
+        let decodeText = '';
         while ((m = regex.exec(input.fileFullPath)) !== null) {
             if (m.index === regex.lastIndex) {
                 regex.lastIndex++;
             }
             console.log('删除时匹配文件名的匹配项目：', m);
-            //选中内容中有几个图片，就把几个图片都删除了
+            //选中内容中有几个图片，就报错，不允许
             let fileFullPath = m[1];
-            let decodeText = decodeURI(fileFullPath);
-			console.log('解码后的Text：', decodeText);
-            try {
-                await this.client.head(decodeText, {});//判断文件是否存在，若不存在，会抛出异常            
-                let result = await this.client.delete(decodeText);
-                console.log(result);
-                delNum++;
-            } catch (e) {
-                output.success = false;
-                output.msg = `${e}`;
-                console.log(e);
-            }
+            decodeText = decodeURI(fileFullPath);
+            matchNum++;
+        }
+        if (matchNum===0) {
+            output.success = false;
+            output.msg = `Content you selected isn't a valid markdown image format!`;
+            return output;
+        }
+        else if(matchNum > 1){
+            output.success = false;
+            output.msg = `You can noly select one markdown image, but you selected ${matchNum}!`;
+            return output;
         }
 
-        if (delNum == 0) {
+        console.log('解码后的Text：', decodeText);
+        try {
+            await this.client.head(decodeText, {});//判断文件是否存在，若不存在，会抛出异常            
+            let result = await this.client.delete(decodeText);
+        } catch (e) {
             output.success = false;
-            output.msg = `Content you selected isn't a valid markdown image format!`
-        }
-        else{
-            output.msg = `You have delete ${delNum} image(s)`
+            output.msg = `${e}`;
         }
         
         return output;
